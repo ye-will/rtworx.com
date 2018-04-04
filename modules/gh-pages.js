c6c7d48b50c89aa4847f8cf4cbfe528bfcb5b8c2
yewei <yewei@nibirutech.com>
2018-04-04 15:08:28 +0800

    fix .nojekyll && add CNAME

--- modules/gh-pages.js
+++ modules/gh-pages.js
@@ -3,7 +3,7 @@ let ghpages = require('gh-pages')
 module.exports = function () {
   this.nuxt.hook('generate:done', async () => {
     await new Promise((resolve, reject) => {
-      ghpages.publish('dist', err => {
+      ghpages.publish('dist', {dotfiles: true}, err => {
         if (typeof err !== undefined) {
           console.log('gh-pages published.')
           resolve()
